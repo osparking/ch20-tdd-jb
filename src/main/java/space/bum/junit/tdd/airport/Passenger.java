5ee26f184c4b2a759e887eff32af29cc3e3ac7e4
osparking <jbpark03@gmail.com>
2024-02-08 20:33:42 +0900

    승객 클래스 - 2 멤버

---
+++ src/main/java/space/bum/junit/tdd/airport/Passenger.java
@@ -0,0 +1,11 @@
+package space.bum.junit.tdd.airport;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Passenger {
+  private String name;
+  private boolean vip;
+}
